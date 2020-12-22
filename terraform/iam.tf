f738448f4b6681dd7bd010f264330b3f81c97d76
zack-klein <klein.zachary.j@gmail.com>
2020-12-22 09:29:58 -0600

    feat: Basic setup for flask dev server on EC2 instance

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,17 +3,17 @@ resource "aws_iam_role" "role" {
 
   assume_role_policy = <<POLICY
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
 }
 POLICY
 }
--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,8 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -ex
 
-sudo yum update -y
-sudo yum install python3 git -y
-sudo pip3 install supervisor
+yum update -y
+yum install python3 git -y
+pip3 install supervisor
+
+cp /usr/local/bin/supervisord /bin/supervisord
+
+# Download the code
+VERSION=master  # TODO: Can be changed later if needed
+HOME=/home/ec2-user
+BACKEND=$HOME/erasmo/backend
+cd $HOME
+git clone https://github.com/zack-klein/erasmo.git
+cd $BACKEND
+
+# Install requirements
+python3 -m venv venv
+$BACKEND/venv/bin/pip3 install -r requirements.txt
+
+# Start the server!
+COMMAND="$BACKEND/venv/bin/flask run --host 0.0.0.0 --port=80"
+SUPERVISOR_BASE_PATH=$BACKEND/supervisord
+SUPERVISOR_CONF_PATH=$SUPERVISOR_BASE_PATH/supervisord.conf
+SUPERVISOR_PATH=$(which supervisord)
+
+mkdir -p $SUPERVISOR_BASE_PATH
+
+cat <<EOF > $SUPERVISOR_CONF_PATH
+[supervisord]
+logfile=$SUPERVISOR_BASE_PATH/supervisord.log
+
+[program:app]
+command=$COMMAND
+directory=$BACKEND
+stdout_logfile=$SUPERVISOR_BASE_PATH/app.log
+redirect_stderr=true
+EOF
+
+$SUPERVISOR_PATH -c $SUPERVISOR_CONF_PATH
 
